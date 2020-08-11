8993fb399382f8b1da94556cf37fcda95adf2641
greydongilmore <ggilmore@uwo.ca>
2020-08-11 18:15:10 -0400

    Update edf2bids.py

--- edf2bids/main/edf2bids.py
+++ edf2bids/main/edf2bids.py
@@ -417,7 +417,7 @@ class edf2bids(QtCore.QRunnable):
 								else:
 									self.bids_settings['json_metadata']['EpochLength'] = 0
 							
-							bids_helper.write_scans(data_fname.split(isub+'/')[-1], file_data[0][irun], self.offset_date)
+							bids_helper.write_scans(data_fname.split(isub+os.path.sep)[-1], file_data[0][irun], self.offset_date)
 							
 							bids_helper.write_channels(file_data[0][irun])
 							bids_helper.write_sidecar(file_data[0][irun])
